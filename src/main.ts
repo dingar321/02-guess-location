1c212a131ecbee9fd8be50fdcb2d1b42119c0269
Dino Garić <dinogaric1@gmail.com>
2022-05-07 17:18:26 +0200

    Fixed the global pipes validation
    Wasn't getting any validations for passwords and such

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    //Unwanted and invalid properties
+    //when creating new users (registering !)
+    whitelist: true,
+    //If enabled it will return a bad request
+    forbidNonWhitelisted: true,
+    transform: true,
 
+  }));
 
   app.use(cookieParser());
 
